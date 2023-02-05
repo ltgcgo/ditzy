290388c2e24878695015bd9d6b698d9068a760b1
Lumière Élevé <88174309+PoneyClairDeLune@users.noreply.github.com>
2023-02-05 15:34:12 +0000

    Basic repeater.

--- docs/README.md
+++ docs/README.md
@@ -3,6 +3,10 @@
 * `client.js`: Library for communicating with a Ditzy server.
 * `server.js`: Library for accepting Ditzy connections from a client.
 
+## CLI
+* `node.js`: Client + server for Node.js.
+* `deno.js`: Client + server for Deno.
+
 ## Data
 * [Binary format](data/binfmt.md)
 * [HTTP requests and responses](data/http.md)
---
+++ src/repeater/index.js
@@ -0,0 +1,34 @@
+"use strict";
+
+// A repeater for Deno, testing whether bidirectional data delivery works.
+
+let serveOpt = {
+	port: 8002
+};
+if (eG("LISTEN_PORT")) {
+	serveOpt.port = parseInt(eG("LISTEN_PORT"));
+};
+
+let handler = async function (request, connInfo) {
+	if (request.headers?.has("Upgrade")) {
+		const {socket, response} = Deno.upgradeWebSocket(request);
+		socket.binaryType = "arraybuffer";
+		socket.addEventListener("open", () => {
+			socket.send("Repeater hello.");
+		});
+		socket.addEventListener("message", (msg) => {
+			socket.send(msg.data);
+			console.info(`Repeated ${msg.data.length} bytes.`);
+		});
+		return response;
+	} else {
+		return new Response(`<!DOCTYPE html><head><style>html{background:#000}</style><script>self.ws=new WebSocket("ws://127.0.0.1:${serveOpt.port}");ws.addEventListener("close",()=>{setTimeout(()=>{location.reload()},4000)})</script></head><body></body>`, {
+				headers: {
+					"content-type": "text/html"
+				}, status: 200
+			}
+		);
+	};
+};
+
+serve(handler, serveOpt);
