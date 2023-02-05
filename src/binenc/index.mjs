14e7df75a870e334838cc147a15dfa77eb8ea329
Lumière Élevé <88174309+PoneyClairDeLune@users.noreply.github.com>
2023-02-05 16:57:28 +0000

    BigInt support for VLV.

--- src/binenc/index.mjs
+++ src/binenc/index.mjs
@@ -4,3 +4,25 @@ import {
 	VLVEncoder,
 	VLVDecoder
 } from "../vlv/index.mjs";
+
+let syxStart = new Uint8Array([240, 126, 127, 4, 64]),
+syxEnd = new Uint8Array([247]);
+
+let DitzyEncoder = class {
+	#vlvEnc = new VLVEncoder(6);
+	#vlvDec = new VLVDecoder(6);
+	pool = [];
+	finalize () {
+		let proxy = [];
+		// Encode each part.
+		// Done.
+		delete this.pool;
+		this.pool = [];
+		return new Blob(proxy);
+	};
+	constructor () {};
+};
+
+export {
+	DitzyEncoder
+};
--- src/vlv/index.mjs
+++ src/vlv/index.mjs
@@ -1,8 +1,18 @@
 "use strict";
 
-class VLVDecoder {
+let bitLength = function (bint) {
+	let len = 0n;
+	while (bint > 0) {
+		bint = bint >> 1n;
+		len ++;
+	};
+	return len - 1n;
+};
+
+let VLVDecoder = class {
 	bits = 7;
 	#value = 0;
+	#vbint = 0n;
 	decode (byte) {
 		if (this.bits > 7) {
 			this.bits = 7;
@@ -21,12 +31,32 @@ class VLVDecoder {
 			return resp;
 		};
 	};
+	decodeBint (bytes) {
+		if (this.bits > 7) {
+			this.bits = 7;
+		};
+		if (bytes < 0) {
+			throw RangeError("Cannot be negative values");
+		};
+		let bits = BigInt(this.bits),
+		byte = BigInt(bytes);
+		let bitLimit = (1n << bits) - 1n;
+		this.#vbint = this.#vbint << bits;
+		if (byte >= bitLimit) {
+			this.#vbint += byte & bitLimit;
+			return null;
+		} else {
+			let resp = this.#vbint + byte;
+			this.#vbint = 0n;
+			return resp;
+		};
+	};
 	constructor (bitLen) {
 		this.bits = bitLen || 7;
 	};
 };
 
-class VLVEncoder {
+let VLVEncoder = class {
 	bits = 7;
 	encode (integer) {
 		if (this.bits > 7) {
@@ -36,7 +66,7 @@ class VLVEncoder {
 			throw RangeError("Cannot be negative values");
 		};
 		let bitLimit = (1 << this.bits) - 1,
-		byteSeq = new Uint8Array(Math.floor(Math.log2(integer) / 7 + 1));
+		byteSeq = new Uint8Array(Math.floor(Math.log2(integer) / this.bits + 1));
 		for (let shift = 0; shift < byteSeq.length; shift ++) {
 			let target = byteSeq.length - 1 - shift,
 			baseVal = bitLimit + 1;
@@ -44,6 +74,28 @@ class VLVEncoder {
 				baseVal = 0;
 			};
 			byteSeq[target] = baseVal + (integer >> (shift * this.bits) & bitLimit);
+		};
+		return byteSeq;
+	};
+	encodeBint (integer) {
+		if (this.bits > 7) {
+			this.bits = 7;
+		};
+		if (integer < 0n) {
+			throw RangeError("Cannot be negative values");
+		};
+		let bits = BigInt(this.bits);
+		let bitLimit = (1n << bits) - 1n,
+		bitLim = Number(bitLimit),
+		byteLen = bitLength(integer) / bits + 1n,
+		byteSeq = new Uint8Array(Number(byteLen));
+		for (let shift = 0n; shift < byteLen; shift ++) {
+			let target = Number(byteLen - 1n - shift),
+			baseVal = bitLim + 1;
+			if (shift == 0) {
+				baseVal = 0;
+			};
+			byteSeq[target] = baseVal + Number(integer >> (shift * bits) & bitLimit);
 		};
 		return byteSeq;
 	};
